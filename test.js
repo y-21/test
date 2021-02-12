9d5e7623b9ce6bc69d0be1caa0e7dfd715af4212
y-21 <bruiny@126.com>
2021-02-12 19:11:16 +0800

    增加cs和js文件

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-12 19:07:23
+ * @LastEditTime: 2021-02-12 19:09:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \test\test.js
+ */
+console.log("这是测试代码");
